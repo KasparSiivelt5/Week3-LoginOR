27bb3824a47ca79019a0e5a091649c6eec1e557a
KasparSiivelt5 <89830554+KasparSiivelt5@users.noreply.github.com>
2021-09-14 12:16:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta kasutajatunnus");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta parool:");
+            string userPassword = Console.ReadLine();
+
+            if (userName != "admin" || userPassword != "admin1234")
+            {
+                Console.WriteLine("Sisselogimine ebaõnnestus.Proovi uuesti.");
+            }
+            else
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+        }
+    }
+}
